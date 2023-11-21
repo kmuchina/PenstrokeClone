8c5463ff6ea3fac9b82ecdb69794f24e43310197
MACHMOSES <machariamoses39@gmail.com>
2023-11-21 12:44:40 +0300

    alingning the images well

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,16 +1,16 @@
 import "./Aside.css"
 import img28 from "../assets/images/WinnerBADGES2019_300px (1).webp"
-// import img1 from "../assets/images/mosee.webp"
-// import img2 from "../assets/images/josee.webp"
-// import img3 from "../assets/images/coffineby.webp"
+ import img1 from "../assets/images/mosee.webp"
+import img2 from "../assets/images/josee.webp"
+import img3 from "../assets/images/coffineby.webp"
 import img27 from "../assets/images/BAKE-Awards-2017-Winners-Badge.webp"
 import img6 from "../assets/images/BAKE-Winner.webp"
 import img7 from "../assets/images/Baesics.webp"
 import img9 from "../assets/images/Holy-Joe.webp"
 import img11 from "../assets/images/The-Human-Temple.webp"
-// import img22 from "../assets/images/smallm.webp"
-// import img23 from "../assets/images/small8445435626_efd474fb3f_b.webp"
-// import img24 from "../assets/images/smallgettyIma.webp"
+import img22 from "../assets/images/smallm.webp"
+import img23 from "../assets/images/small8445435626_efd474fb3f_b.webp"
+import img24 from "../assets/images/smallgettyIma.webp"
 
 
 const Aside = ( ) => {
@@ -82,10 +82,10 @@ const Aside = ( ) => {
                     <li><a href="/">COMMENT</a></li>
                 
             </ul>
-        {/* <section>
+         <section>
             <div>
                 <img src= {img1} alt="josphe with a stike" />
-                <div>
+                <div className="spanh">
                     <span>SEPTEMBER 4, 2023</span>
                     <h5><a href="/">Jesus and Joseph of the Old Testament</a></h5>
                 </div>
@@ -93,7 +93,7 @@ const Aside = ( ) => {
             <div>
                 
                 <img src= {img3}alt=" a coffine"/>
-                <div>
+                <div className="spanh">
                     <span>AUGUST 13, 2023</span>
                     <h5><a href="/">Eulogy: the Death of Self</a></h5>
                 </div>
@@ -103,7 +103,7 @@ const Aside = ( ) => {
             <div>
                 
                 <img src= {img2} alt=" two people a man and woman"/>
-                <div>
+                <div className="spanh">
                     <span>JULY 12, 2023</span>
                     <h5><a href="/">Let your heart be ready</a></h5>
                 </div>
@@ -112,7 +112,7 @@ const Aside = ( ) => {
             <div>
                 
                 <img src= {img22} alt=" a cross and person raising hands"/>
-                <div>
+                <div className="spanh">
                     <span>APRIL 26, 2023</span>
                     <h5><a href="/">12 things to consider on the first date
                     </a></h5>
@@ -121,7 +121,7 @@ const Aside = ( ) => {
             </div>
             <div>
                 <img src= {img24} alt="JBFhdhvjhajhV"/>
-                <div>
+                <div className="spanh">
                     <span>APRIL 11, 2023</span>
                     <h5><a href="/">What is the spiritual act of worship in Romans 12?</a></h5>
                 </div>
@@ -130,13 +130,13 @@ const Aside = ( ) => {
             <div>
                 
                 <img src= {img23} alt="one of the best images"/>
-                <div>
+                <div className="spanh">
                     <span>MARCH 13, 2023</span>
                     <h5><a href="/">Your life is a breath.</a></h5>
                 </div>
                 
-            </div>  
-        </section> */}
+            </div> 
+        </section>
 
             <h3>We are members!</h3>
         </aside>
